5ec05144eba9921f98994728c2026ed4c6dfdf2c
ram-ismael <ramadan.i.a.ismael@gmail.com>
2026-08-17 21:31:01 +0200

    feat(navbar): add theme selection menu
    Co-authored-by: ismael_RAM <ramadan.ismael02@gmail.com>

---
+++ src/Views/CustomTheme/CustomThemeDialogView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace SukiUI_Demo.Views;
+
+public partial class CustomThemeDialogView : UserControl
+{
+    public CustomThemeDialogView()
+    {
+        InitializeComponent();
+    }
+}
